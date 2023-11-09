32a0e61b694eec9acd75a5090776cdf9c5fdfce9
Valerian <@VelValerian>
2023-11-09 08:09:48 +0200

    Basic structure

---
+++ query_data/binance.py
@@ -0,0 +1,12 @@
+
+
+# Функции получения данных с определенной биржи
+
+
+# # Create an async function to retrieve klines for a given interval
+# async def get_klines():
+#     klines = client.continuous_klines(pair=symbol,
+#                                       contractType='PERPETUAL',
+#                                       interval=interval,
+#                                       startTime=1602748800000,
+#                                       limit=11)
